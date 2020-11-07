fa26bd46eeafcb0def678b7c993dda573838fca2
shwetachaturvedi <73924330+shwetachaturvedi@users.noreply.github.com>
2020-11-07 11:26:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -70,7 +70,7 @@ function setup() {
 
 function draw() {
   //trex.debug = true;
-  background("pink");
+  background("light green");
   text("Score: "+ score, 500,50);
   
   if (gameState===PLAY){
